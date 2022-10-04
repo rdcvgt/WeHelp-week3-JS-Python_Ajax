106fe3eb82a83ba271551516d250b84de318c0ef
彭勝緯 <rdcvgt963@MacBook-Air.hitronhub.home>
2022-10-04 16:10:57 +0800

    add part4

---
+++ Part4-1/JS/index.js
@@ -0,0 +1,26 @@
+const menuBtn = document.querySelector('.phoneMenu');
+const sidebar = document.querySelector('.sidebar');
+const closeImg = document.querySelector('.closeImg');
+const spacebar = document.querySelector('.spacebar')
+const sidebarOppend = document.querySelector('.container').style['right'] = "0%"
+
+if(menuBtn){
+	menuBtn.addEventListener('click', () =>{
+			sidebar.style["animation"]= "moveSidebar 0.5s both";
+	});
+}
+
+
+
+if(sidebarOppend){
+	if(closeImg){
+	closeImg.addEventListener('click', () =>{
+			sidebar.style["animation"]= "moveSidebarReverse 0.5s both";
+	});
+}
+	if(spacebar){
+	spacebar.addEventListener('click', () =>{
+			sidebar.style["animation"]= "moveSidebarReverse 0.5s both";
+	});
+}
+}
